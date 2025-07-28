d470abe8329594a2214a186cfdb59002841b3e80
SurajBelduha <163997594+SurajBelduha@users.noreply.github.com>
2025-07-28 23:05:51 +0530

    Update TestContext.cs

--- AMS/AMS.Repository/Models/TestContext.cs
+++ AMS/AMS.Repository/Models/TestContext.cs
@@ -23,7 +23,7 @@ namespace AMS.Repository.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=LAPTOP-BT9E70FJ\\SQLEXPRESS;Database=Test;MultipleActiveResultSets=True;Trusted_Connection=True;TrustServerCertificate=True;");
+                optionsBuilder.UseSqlServer("Server=test;Database=Test;MultipleActiveResultSets=True;Trusted_Connection=True;TrustServerCertificate=True;");
             }
         }
 
